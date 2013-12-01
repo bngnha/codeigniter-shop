ff29f7b93f956129555453819157494e9de2a5c6
bngnha <bngnha@gmail.com>
2013-12-01 14:53:57 +0700

    Update source code

--- application/core/Admin_Controller.php
+++ application/core/Admin_Controller.php
@@ -4,8 +4,8 @@ class Admin_Controller extends MX_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->data = array();
-		$this->error= array();
+		$this->data  = array();
+		$this->error = array();
 
 		// Load theme and template
 		$this->load->helper('themes');
@@ -14,17 +14,17 @@ class Admin_Controller extends MX_Controller
 		$this->load->library('Asset');
 
 		// Work out module, controller and method and make them accessable throught the CI instance
-		$this->module = $this->module = $this->router->fetch_module();
+		$this->module     = $this->module = $this->router->fetch_module();
 		$this->controller = $this->controller = $this->router->fetch_class();
-		$this->method = $this->method = $this->router->fetch_method();
-		
+		$this->method     = $this->method = $this->router->fetch_method();
+
 		// Get meta data for the module
 		$this->template->module_details = $this->module_details = $this->module_details = $this->module;
 
-		Asset::add_path('module', APPPATH.'modules/'.$this->module.'/views/');
+		Asset::add_path('module', APPPATH . 'modules/' . $this->module . '/views/');
 
 		// Theme directory path
-		Asset::add_path('theme', APPPATH.'themes/admin/');
+		Asset::add_path('theme', APPPATH . 'themes/admin/');
 		Asset::set_path('theme');
 
 		/*
@@ -49,10 +49,9 @@ class Admin_Controller extends MX_Controller
 		$this->load->model('user/user_model');
 		*/
 
-
 		// Load language
 		//Get language that was set in db
-		$lang_cd = 'en';
+		$lang_cd   = 'en';
 		$lang_name = 'english';
 
 		$this->load->model('setting/setting_model');
@@ -66,35 +65,35 @@ class Admin_Controller extends MX_Controller
 			}
 		}
 		$this->session->set_userdata('config_admin_language', $lang_cd);
-		
-         // Get language id from language table and set to session
-        $where = array();
-        $where['status'] = 1;
-        $where['code']   = $lang_cd;
-        $this->db->select('language_id');
-        $query = $this->db->get_where('language', $where);
-        $result = $query->row_array();
 
-        if (isset($result['language_id']))
-        {
-            $this->session->set_userdata('config_language_id', $result['language_id']);
-        }
-        
-        // Load config
-        //$this->load->helper('setting');
-        //get_setting_to_session();
+		// Get language id from language table and set to session
+		$where           = array();
+		$where['status'] = 1;
+		$where['code']   = $lang_cd;
+		$this->db->select('language_id');
+		$query  = $this->db->get_where('language', $where);
+		$result = $query->row_array();
 
-        // Load information for header
-        $this->data['logo_img'] = base_url() . 'uploads/' . $this->session->userdata('config_logo');
-        
-        // Load menu
-        //$this->load->helper('menu_page');
+		if (isset($result['language_id']))
+		{
+			$this->session->set_userdata('config_language_id', $result['language_id']);
+		}
+
+		// Load config
+		//$this->load->helper('setting');
+		//get_setting_to_session();
+
+		// Load information for header
+		$this->data['logo_img'] = base_url() . 'uploads/' . $this->session->userdata('config_logo');
+
+		// Load menu
+		//$this->load->helper('menu_page');
 		//load_header_menu($this->data);
-        
-        // Load footer
-        $this->data['shop_name'] = $this->session->userdata('config_name');
-        $this->data['shop_address'] = $this->session->userdata('config_address');
-        $this->data['shop_telephone'] = $this->session->userdata('config_telephone');
-        $this->data['shop_email'] = $this->session->userdata('config_email');
-    }
+
+		// Load footer
+		$this->data['shop_name']      = $this->session->userdata('config_name');
+		$this->data['shop_address']   = $this->session->userdata('config_address');
+		$this->data['shop_telephone'] = $this->session->userdata('config_telephone');
+		$this->data['shop_email']     = $this->session->userdata('config_email');
+	}
 }
